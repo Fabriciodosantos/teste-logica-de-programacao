32bd45e92fc19b53c516341de2c43380b1ed488b
Fabriciodossantos <fsan4344@gmail.com>
2023-03-01 21:52:27 -0300

    finalizando teste sequencia de fibonacci

--- sequencia-fibonacci.php
+++ sequencia-fibonacci.php
@@ -1,4 +1,32 @@
 <?php
+function fibonacci ($n)
+{
+    $a = 0;
+    $b = 1;
+    $pertence = false;
+    $naoPertence =false;
+
+    for ($i=0; $i <= $n; $i++) { 
+        $c = $a + $b;
+        $a = $b;
+        $b = $c;
+
+        if ($c === $n){
+            $pertence = true;
+        } else {
+            $naoPertence = true;
+        }
+    }
+
+    if ($pertence){
+        return " O número $n pertence a sequencia de fibonacci.";
+    } else if ($naoPertence) {
+        return "O número $n não pertence a sequencia de fibonacci.";
+    }
+}
+    
+
+echo fibonacci(144);
 
 
 
